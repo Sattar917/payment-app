268ffaaf8114d8c6a2289088713198b06451b783
Sattar <sattar.gadjiev@gmail.com>
2025-02-02 16:08:30 +0400

    Dockerfile and Docker-compose added, tested and fixed bugs, application runnable

--- ms-transfer/src/main/resources/db/changelog/v0.0.1/02-02-25-create-account-table.sql
+++ ms-transfer/src/main/resources/db/changelog/v0.0.1/02-02-25-create-account-table.sql
@@ -4,7 +4,7 @@ CREATE TABLE accounts
     user_id        BIGINT NOT NULL,
     balance        NUMERIC NOT NULL DEFAULT 100.00,
     phone_number   VARCHAR(255) NOT NULL,
-    birth_date     TIMESTAMP
+    birth_date     TIMESTAMP,
     created_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at     TIMESTAMP
 );
