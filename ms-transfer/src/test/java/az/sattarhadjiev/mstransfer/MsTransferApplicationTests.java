dd6ad34c20deda3375c8c6f6e736f89709112fbf
Sattar <sattar.gadjiev@gmail.com>
2025-02-02 13:51:59 +0400

    Project created

---
+++ ms-transfer/src/test/java/az/sattarhadjiev/mstransfer/MsTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package az.sattarhadjiev.mstransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
